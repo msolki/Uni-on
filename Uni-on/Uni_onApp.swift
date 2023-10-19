e9daea6f8e0c8dfaa7a6eabe7324d1979d6bf98d
Mohammad Solki <53335722+msolki@users.noreply.github.com>
2023-10-19 11:49:38 +0200

    Initial Commit

---
+++ Uni-on/Uni_onApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uni_onApp.swift
+//  Uni-on
+//
+//  Created by Mohammad Solki on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Uni_onApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
